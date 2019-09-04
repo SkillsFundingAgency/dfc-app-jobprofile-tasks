2f56d05a49802dd9734f819b45a78eb0594d8069
Ilyas Dhin <ilyasdhin@gmail.com>
2019-09-04 14:53:31 +0100

    Added cosmos

---
+++ DFC.App.JobProfileTasks.Data/Contracts/IDraftJobProfileOverviewSegmentService.cs
@@ -0,0 +1,10 @@
+﻿using DFC.App.JobProfileTasks.Data.Models;
+using System.Threading.Tasks;
+
+namespace DFC.App.JobProfileOverview.Data.Contracts
+{
+    public interface IDraftJobProfileOverviewSegmentService
+    {
+        Task<JobProfileTaskSegmentModel> GetSitefinityData(string canonicalName);
+    }
+}
---
+++ DFC.App.JobProfileTasks.Data/Models/JobProfileTaskSegmentModel.cs
@@ -0,0 +1,24 @@
+﻿using DFC.App.JobProfileTasks.Data.Contracts;
+using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DFC.App.JobProfileTasks.Data.Models
+{
+    public class JobProfileTaskSegmentModel : IDataModel
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid DocumentId { get; set; }
+
+        [Required]
+        [JsonProperty(PropertyName = "canonicalName")]
+        public string CanonicalName { get; set; }
+
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+
+        [Display(Name = "Last Reviewed")]
+        [JsonProperty(PropertyName = "lastReviewed")]
+        public DateTime LastReviewed { get; set; }
+    }
+}
