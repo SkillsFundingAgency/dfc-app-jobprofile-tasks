f2c5df02d1c4e9743d3a72422c78b7a41d55af56
Ilyas Dhin <ilyasdhin@gmail.com>
2019-09-05 15:51:11 +0100

    Added more tests

---
+++ DFC.App.JobProfileTasks.UnitTests/ControllerTests/HomeControllerTests/HomeControllerErrorTests.cs
@@ -0,0 +1,26 @@
+﻿using DFC.App.JobProfileTasks.ViewModels;
+using FakeItEasy;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace DFC.App.JobProfileTasks.UnitTests.ControllerTests.HomeControllerTests
+{
+    public class HomeControllerErrorTests : BaseHomeController
+    {
+        [Theory]
+        [MemberData(nameof(HtmlMediaTypes))]
+        public void HomeControllerErrorHtmlReturnsSuccess(string mediaTypeName)
+        {
+            // Arrange
+            var controller = BuildHomeController(mediaTypeName);
+
+            // Act
+            var result = controller.Error();
+
+            // Assert
+            Assert.IsType<ViewResult>(result);
+
+            controller.Dispose();
+        }
+    }
+}
