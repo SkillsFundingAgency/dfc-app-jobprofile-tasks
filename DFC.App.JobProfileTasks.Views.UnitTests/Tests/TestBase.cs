34e14a51ed9f56e8499399e3f3c52071b1622bc4
ilyasdhin <ilyasdhin@gmail.com>
2019-11-19 11:16:28 +0000

    Reoved appsettings.json file and exposed viewpath as property on testbase (#44)

--- DFC.App.JobProfileTasks.Views.UnitTests/Tests/TestBase.cs
+++ DFC.App.JobProfileTasks.Views.UnitTests/Tests/TestBase.cs
@@ -6,21 +6,11 @@ namespace DFC.App.JobProfileTasks.Views.UnitTests.Tests
 {
     public class TestBase
     {
-        private readonly string viewRootPath;
         private readonly IConfigurationRoot configuration;
 
-        public TestBase()
-        {
-            var config = new ConfigurationBuilder();
-            config.AddJsonFile("appsettings.json");
-            configuration = config.Build();
-
-            viewRootPath = configuration["ViewRootPath"];
-        }
-
-        public string ViewRootPath => viewRootPath;
-
         protected string CurrencySymbol => CultureInfo.CurrentCulture.NumberFormat.CurrencySymbol;
+
+        protected string ViewRootPath => "..\\..\\..\\..\\DFC.App.JobProfileTasks\\";
 
         protected string HtmlEncode(string value)
         {
