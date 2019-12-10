9c96f117bae3491d414e7c4e09c2ca65a9090410
ilyasdhin <ilyasdhin@gmail.com>
2019-12-10 11:47:27 +0000

    Fixes for ensuring that translator methods are only applicable for ap… (#57)
    * Fixes for ensuring that translator methods are only applicable for api models
    
    * Added [ExcludeFromCodeCoverage] to formatters
    
    * Added [ExcludeFromCodeCoverage] to mapper profiles

--- DFC.App.JobProfileTasks.SegmentService/IFormatContentService.cs
+++ DFC.App.JobProfileTasks.SegmentService/IFormatContentService.cs
@@ -1,9 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using DFC.HtmlToDataTranslator.Contracts;
+using System.Collections.Generic;
 
 namespace DFC.App.JobProfileTasks.SegmentService
 {
     public interface IFormatContentService
     {
-        string GetParagraphText(string openingText, IEnumerable<string> dataItems, string separator);
+        string GetParagraphText(string openingText, IEnumerable<string> dataItems, string separator, IHtmlToDataTranslator htmlToDataTranslator);
     }
 }
--- DFC.App.JobProfileTasks/AutoMapperProfiles/ValueConverters/LocationFormatter.cs
+++ DFC.App.JobProfileTasks/AutoMapperProfiles/ValueConverters/LocationFormatter.cs
@@ -1,17 +1,27 @@
 ﻿using AutoMapper;
 using DFC.App.JobProfileTasks.SegmentService;
+using DFC.HtmlToDataTranslator.Contracts;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.App.JobProfileTasks.AutoMapperProfiles.ValueConverters
 {
+    [ExcludeFromCodeCoverage]
     public class LocationFormatter : FormatContentService, IValueConverter<IEnumerable<string>, string>
     {
+        private readonly IHtmlToDataTranslator htmlToDataTranslator;
+
+        public LocationFormatter(IHtmlToDataTranslator htmlToDataTranslator)
+        {
+            this.htmlToDataTranslator = htmlToDataTranslator;
+        }
+
         public string Convert(IEnumerable<string> sourceMember, ResolutionContext context)
         {
             const string LocationLeadingText = "You could work";
             const string LocationConjunction = "or";
 
-            return GetParagraphText(LocationLeadingText, sourceMember, LocationConjunction);
+            return GetParagraphText(LocationLeadingText, sourceMember, LocationConjunction, htmlToDataTranslator);
         }
     }
 }
